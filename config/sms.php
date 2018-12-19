d6fccb81e31fca7e2e37e16a9f9f0ebdaebfd8a8
陈志云 <357365507@qq.com>
2018-12-19 16:05:11 +0800

    edit config

--- config/sms.php
+++ config/sms.php
@@ -3,15 +3,15 @@ return [
     /**
      * 短信用户名
      */
-    'username'=>'xiamo1987',
+    'username'=>'test',
     /**
      * 短信密码
      */
-    'password'=>'ABC19875154abc',
+    'password'=>'123456',
     /**
      * 短信模板
      */
     'templates'=>[
-        'captcha'=>415301
+        'captcha'=>11111
     ]
 ];
